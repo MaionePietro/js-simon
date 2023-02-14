cc5fba2f0e104613dc19e6a964fca535b0f8b6d9
MaionePietro <pietromaionespam@gmail.com>
2023-02-14 09:53:51 +0100

    FEAT function comparatore

--- js/app.js
+++ js/app.js
@@ -2,21 +2,27 @@
 //alert('memorizza i seguenti numeri: '+ random());
 const countdown = document.getElementById('countdown');
 
+let arrayNumeri = [];
+let arrayNumeriInput = [];
 let i = 30;
 const clock = setInterval(myFuction, 1000);
+random();
 
-function myFuction(){
+
+//COUNTDOWN
+function myFuction() {
     countdown.innerHTML = i;
-    if(i===0){
-        clearInterval(clock)
-    }else{
+    if (i === 0) {
+        clearInterval(clock);
+        numberInput();
+        comparatore();
+    } else {
         i--;
     }
 }
 
-
+//FUNZIONE PER NUMERI RANDOMICI
 function random() {
-    let arrayNumeri = [];
     while (arrayNumeri.length < 5) {
         let randomNumber = Math.floor(Math.random() * 100) + 1;
         if (arrayNumeri.indexOf(randomNumber) === -1) {
@@ -24,5 +30,23 @@ function random() {
         }
     }
     console.log(arrayNumeri);
-    return arrayNumeri;
+}
+
+//FUNZIONE PER FAR INSERIRE NUMERI ALL'UTENTE
+function numberInput() {
+    while (arrayNumeriInput.length < 5) {
+        arrayNumeriInput.push(parseInt(prompt('vediamo cosa ti ricordi')));
+    }
+    console.log(arrayNumeriInput);
+}
+
+//FUNZIONE PER COMPARARE NUMERI
+function comparatore() {
+    for (cont = 0; cont < arrayNumeri.length; cont++) {
+        if (arrayNumeri.includes(arrayNumeriInput[cont])) {
+            console.log("Il numero è presente: "+arrayNumeriInput[cont]);
+        } else {
+            console.log("Il numero non è presente nell'array.");
+        }
+    }
 }
